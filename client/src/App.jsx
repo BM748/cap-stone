31ed8bbe65bfdd2219ff76f80bd83d4e2d4b53e3
BM7487 <bmm7682@gmail.com>
2023-11-11 16:54:47 -0600

    fixed spelling for components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,11 @@ import {Route,Routes} from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import SignIn  from './componets/login';
-import SignUp from './componets/signupArtist';
-import Selection from './componets/signInSelection';
-import SignUser from './componets/signupUser';
-import Home from './componets/home';
+import SignIn  from './components/login';
+import SignUp from './components/signupArtist';
+import Selection from './components/signInSelection';
+import SignUser from './components/signupUser';
+import Home from './components/home';
 
 function App() {
   const [token, setToken] = useState(true);
