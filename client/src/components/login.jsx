9a48f3021c3f164e6f8b98a3c3d45901bf0ba2aa
BM7487 <bmm7682@gmail.com>
2023-11-14 01:06:32 -0600

    added checkbox for user to either login as artist and patron.

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -9,50 +9,78 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import React, {useState} from 'react';
 
-export default function SignIn({setToken}) {
-  const navigate = useNavigate()
+export default function SignIn({ setToken }) {
+  const navigate = useNavigate();
+  const [isArtist, setIsArtist] = useState(false);
+  const [isPatron, setIsPatron] = useState(false);
+  
 
-    const handleSubmit = (event) =>{
-        event.preventDefault();
-        const data = new FormData (event.currentTarget);
+  const handleCheckboxChange = (type) => {
+    if (type === 'artist') {
+      setIsArtist(!isArtist);
+      setIsPatron(false);
+    } else if (type === 'patron') {
+      setIsPatron(!isPatron);
+      setIsArtist(false);
+    }
+  };
 
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
 
-            const email = data.get("email");
-            const password = data.get("password");
-      
+      const email = data.get("email");
+      const password = data.get("password");
 
-        // assumes user is artist
-        axios.post('http://localhost:5081/api/login/artist', {
-          emailId: email,
-          password,
-      }) .then((result) => {
-        console.log(result)
-        const token = result.data.loggedIn;
+      if (isArtist) {
+        const artistResult = await axios.post(
+          'http://localhost:5081/api/login/artist',
+          { emailId: email, password }
+        );
 
-        setToken(token);
-        
-        if (token){
+        const artistToken = artistResult.data.loggedIn;
+        setToken(artistToken);
+
+        if (artistToken) {
           navigate('/');
+          return;
         }
+      } else if (isPatron) {
+        const patronResult = await axios.post(
+          'http://localhost:5081/api/login/user',
+          { emailId: email, password}
+          
+        );
 
-      }) .catch(e => console.error(e))
+        const patronToken = patronResult.data.loggedIn;
+        setToken(patronToken);
 
+        if (patronToken) {
+          navigate('/');
+          return;
+        }
+      } else {
+        console.error("Please select either 'Artist' or 'Patron'.");
       }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-        
-    return (
-        <Container component="main" maxWidth="xs">
-            <Box
-            sx={{
-                marginTop: 8,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-            }}
-            >
-
-<Typography component="h1" variant="h5">
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
           Sign in
         </Typography>
         <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
@@ -77,8 +105,26 @@ export default function SignIn({setToken}) {
             autoComplete="current-password"
           />
           <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
+            control={
+              <Checkbox
+                value="artist"
+                color="primary"
+                checked={isArtist}
+                onChange={() => handleCheckboxChange('artist')}
+              />
+            }
+            label="Artist"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                value="patron"
+                color="primary"
+                checked={isPatron}
+                onChange={() => handleCheckboxChange('patron')}
+              />
+            }
+            label="Patron"
           />
           <Button
             type="submit"
@@ -95,7 +141,7 @@ export default function SignIn({setToken}) {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/signInSelection" variant="body2">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
