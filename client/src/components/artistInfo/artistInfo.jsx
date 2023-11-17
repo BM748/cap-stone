8e8b85eab0a66ee4cc06fcb4c5440a6c79e6a915
BM7487 <bmm7682@gmail.com>
2023-11-16 19:47:00 -0600

    add artistInfo

---
+++ client/src/components/artistInfo/artistInfo.jsx
@@ -0,0 +1,13 @@
+import "./artistInfo.scss"
+
+function ArtistInfo(){
+    return(
+        <div className="Main-ArtistInfo-Container">
+            Info
+
+        </div>
+    )
+}
+
+
+export default ArtistInfo;
