9cfec8f4db21e683392b68b3610d8d97ff696aca
BM7487 <bmm7682@gmail.com>
2023-11-17 22:43:26 -0600

    add local storage code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,36 @@
-import { useState } from 'react'
-import {Route,Routes} from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import SignIn  from './components/login';
+import { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SignIn from './components/login';
 import SignUp from './components/signupArtist';
-import Selection from './components/signInSelection';
-import SignUser from './components/signupUser';
 import Home from './components/home';
+import ProfilePage from './components/profile/profilePage';
+
 
 function App() {
-  const [token, setToken] = useState();
+  // Check if a token is already present in local storage
+  const initialToken = localStorage.getItem('token');
+  const [token, setToken] = useState(initialToken);
 
-  if(!token) {
-    return <SignIn setToken={setToken}/>
+  // Update local storage when the token changes
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem('token', token);
+    } else {
+      localStorage.removeItem('token');
+    }
+  }, [token]);
+
+  if (!token) {
+    return <SignIn setToken={setToken} />;
   }
+  
 
   return (
-    <>
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/signupArtist" element={<SignUp/>}/>
-      <Route path='/signinSelection' element={<Selection/>}/>
-      <Route path='/signupUser' element={<SignUser/>}/>
-      
+      <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<ProfilePage />} />
     </Routes>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
